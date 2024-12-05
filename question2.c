372840728cd14a7c79bae1dfdafb0b4715932900
FlorianBnGld <161326251+FlorianBnGld@users.noreply.github.com>
2024-12-05 10:05:51 +0100

    Update question2.c

--- question2.c
+++ question2.c
@@ -1,31 +1,31 @@
 #include "question2.h"
 #include "main.h"
+#define MAX_COMMAND_LEN 1024
 
 
 // Read the command
-void read_command(char *command){
-    int bytes_read = read(STDIN_FILENO, command, MAX_COMMAND_LEN);
+char * read_command(){
+    char * command = malloc(MAX_COMMAND_LEN);
+    int bytes_read = read(STDIN_FILENO, command, 1024);
     if (bytes_read > 0){
         if (command[bytes_read-1] == '\n') {
             command[bytes_read-1] = '\0';
         } else {
-            command[bytes_read] = '\0';
+            command='\0';
         }
     }
-    else {
-        EXIT_FAILURE;
-    }
+    return command;
 }
 
 //Execute the command
-void execute_command(char *command, int *status){
+void execute_command(char *command){
+    int status;
     pid_t pid = fork();
-
     if (pid == 0) { // child execute command
         execlp(command, command, NULL);
         exit(1); //make sure to finish child process, in order to avoid child process "superposition"
     }
     else { //father : waiting for child to finish
-        wait(status);
+        waitpid(pid, &status,0);
     }
 }
