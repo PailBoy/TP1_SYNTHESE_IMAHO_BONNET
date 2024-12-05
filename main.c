c1f1fc399f88a40189b2306773ee1b8b39cfea5d
FlorianBnGld <161326251+FlorianBnGld@users.noreply.github.com>
2024-12-05 11:59:13 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,25 @@
 #include "question1.h"
 #include "question2.h"
 #include "question3.h"
+#include "question4.h"
+#include "question5.h"
+
 
 int main(){
+    int status = 0;
     welcome();
+    
     while(1){
         print_prompt();
+        print_status(status);
+        print_time(get_time());
         print_percentage();
         char * command = read_command();
         if (command_exit(command) == 1) { //if the instruction is exit, we stop
             exit(0);
         }
-        if (strlen(read_command()) == 0){
-            write(STDOUT_FILENO, "Bye bye...\n", strlen("Bye bye...\n"));
-            break;
-        }
-        execute_command(command); 
+        start_timer();
+        status = execute_command(command); 
+        end_timer();
     }
-    
 }
